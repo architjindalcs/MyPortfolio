4aaaf553e92f824f60ecfd3fa3102ee44f76e4d8
Archit Jindal <>
2021-05-22 22:15:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const h1=document.querySelector('#desc');
+const desc=
+"Hi, I am Archit Jindal. I am pursuing B.Tech in Computer Science and Engineering (CSE) (Final Year) from NIT Jalandhar. My areas of interest are Data Structures and Algorithms, Problem Solving and Web development.<br> <br> <img id='lin' src='https://img.icons8.com/color/48/000000/linkedin.png/'> <a href=' https://www.linkedin.com/in/archit-jindal-51b88a165/' > Linkedin Profile </a> <br> <img id='git' src='https://img.icons8.com/color/96/000000/github-2.png'> <a href=' https://www.github.com/architjindalcs' > Github Profile </a> <br> <img id='lin' src='https://img.icons8.com/plasticine/100/000000/google-drive.png'> <a href=' https://www.linkedin.com/in/archit-jindal-51b88a165/' > My Resume </a> <br>  "
+const others=document.querySelector('.others');
+// others.style.display='none'
+
+h1.innerHTML=desc
+
+const imgId=document.querySelector('#ilike')
